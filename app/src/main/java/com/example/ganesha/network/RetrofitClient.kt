c49cd40286f99c47e1a501a40f18cc86c7789908
Kevin <welshk91@gmail.com>
2023-01-07 03:02:52 -0500

    Fixed base url

--- app/src/main/java/com/example/ganesha/network/RetrofitClient.kt
+++ app/src/main/java/com/example/ganesha/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    private const val BASE_URL = "https://plaid.com/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
