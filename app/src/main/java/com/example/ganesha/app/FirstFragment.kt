930eb350880cd9ff45cedaa3ccfea4c9c0637fa5
Kevin <welshk91@gmail.com>
2023-01-07 00:59:49 -0500

    Moved Fragments and Activities to app package

--- app/src/main/java/com/example/ganesha/app/FirstFragment.kt
+++ app/src/main/java/com/example/ganesha/app/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ganesha
+package com.example.ganesha.app
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.ganesha.R
 import com.example.ganesha.databinding.FragmentFirstBinding
 
 /**
--- app/src/main/java/com/example/ganesha/app/MainActivity.kt
+++ app/src/main/java/com/example/ganesha/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ganesha
+package com.example.ganesha.app
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -10,6 +10,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.ganesha.R
 import com.example.ganesha.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
