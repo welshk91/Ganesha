9a669d8ced3f2784e1b2f9e9e7c89acd2c23f77b
Kevin <welshk91@gmail.com>
2023-12-02 03:26:31 -0500

    Target sandbox right now. Possibly move this to regular PLAID later...

--- app/src/main/java/com/example/ganesha/network/RetrofitClient.kt
+++ app/src/main/java/com/example/ganesha/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://plaid.com/"
+    private const val BASE_URL = "https://sandbox.plaid.com/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
